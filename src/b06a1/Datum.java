006269f6beac85c2d1c13c8886059cb831456db5
felixkhaw <felix.knupper@haw-hamburg.de>
2026-05-30 12:17:24 +0200

    b06a1

---
+++ src/b06a1/Datum.java
@@ -0,0 +1,46 @@
+package b06a1;
+
+public class Datum {
+    private int t;
+    private int m;
+    private int j;
+
+    public Datum(){
+        this.j = 1;
+        this.m = 1;
+        this.t = 1;
+    }
+
+    public Datum(int j, int m){
+        this.j = 1;
+        this.m = 1;
+        this.t = 1;
+    }
+
+    public Datum(int j, int m, int t){
+        this.j = j;
+        this.m = m;
+        this.t = t;
+    }
+
+    public int getJahr(){
+        return this.j;
+    }
+
+    public int getMonat(){
+        return this.m;
+    }
+
+    public int getTag(){
+        return this.t;
+    }
+
+    public boolean equals(Datum d){
+        return (this.j == d.j & this.m == d.m & this.t == d.t);
+    }
+
+    public boolean istFrueher(Datum d){
+        // Wie kann ich am besten die Abfrage machen?
+        // return (this.j < d.j & this.m < d.m & this.t < d.t);
+    }
+}
---
+++ src/b06a1/DatumTest.java
@@ -0,0 +1,8 @@
+package b06a1;
+
+public class DatumTest {
+    static void main(String[] args) {
+        Datum d = new Datum();
+        System.out.println();
+    }
+}
