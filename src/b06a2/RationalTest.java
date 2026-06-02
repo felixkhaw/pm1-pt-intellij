8e9842007651fc13bef5e1654df78f53dd0df562
felixkhaw <felix.knupper@haw-hamburg.de>
2026-06-02 20:30:49 +0200

    Fertigstellen von Teststruktur

--- src/b06a2/RationalTest.java
+++ src/b06a2/RationalTest.java
@@ -1,9 +1,26 @@
 package b06a2;
 
 public class RationalTest {
+    public static boolean istKleinerCheck(Rational a, Rational b, boolean erw) {
+        boolean erg = a.istKleiner(b);
+        boolean istKorrekt = (erg == erw);
+        if (!istKorrekt) {
+            System.out.println("FEHLER: istKleiner(" + a.toString() + "," + b.toString() + ")==" + erg + " statt " + erw);
+        }
+        return istKorrekt;
+    }
+
+    public static boolean istKleinerTest() {
+        return istKleinerCheck(new Rational(1,2),new Rational(3,4), true);
+    }
+
+    public static boolean test() {
+        return istKleinerTest();
+    }
+
     static void main(String[] args) {
-        Rational r1 = new Rational(5,7);
-        Rational r2 = new Rational(7,5);
-        System.out.println(r1.sub(r2).toString());
+        if (test()) {
+            System.out.println("OK");
+        }
     }
 }
