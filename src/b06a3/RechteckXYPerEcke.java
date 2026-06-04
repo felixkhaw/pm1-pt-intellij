62ab6619a645f411b4420b219324759127186021
felixkhaw <felix.knupper@haw-hamburg.de>
2026-06-04 23:20:54 +0200

    b06a3 Korrigiere RF

--- src/b06a3/RechteckXYPerEcke.java
+++ src/b06a3/RechteckXYPerEcke.java
@@ -3,7 +3,7 @@ package b06a3;
 public class RechteckXYPerEcke {
     private Punkt2D p1;
     private Punkt2D p2;
-    // private final double EPSILON = 0.01; // Rundungsfehler über Math.abs(...) < EPSILON
+    // private final double EPSILON = 0.01; // Rundungstoleranz über Math.abs(...) < EPSILON
     RechteckXYPerEcke(double xl, double xr, double yu, double yo){
         this.p1 = new Punkt2D(xl, yu);
         this.p2 = new Punkt2D(xr, yo);
