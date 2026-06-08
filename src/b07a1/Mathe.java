5ca2e5f578602f29ebe93ad8a2e13a5dd201e16a
felixkhaw <wzr955@haw-hamburg.de>
2026-06-08 22:04:12 +0200

    Ändern von b07a1 addloop

--- src/b07a1/Mathe.java
+++ src/b07a1/Mathe.java
@@ -3,31 +3,14 @@ package b07a1;
 public class Mathe {
     // a=-3 b=-6
     public static int addLoop(int a, int b) {
-        int tmp_a_us = 0;
-        boolean a_us = false;
-        boolean b_us = true;
-        if (a < 0) {
-            tmp_a_us = -(a);
-            a_us = false;
+        while (a > 0) {
+            ++b;
+            --a;
         }
-        if(b<0){
-            b_us = false;
-        }
-        
-        while (tmp_a_us > 0) {
-            if(a_us==false && b_us == false){
-                --b;
-                --tmp_a_us;
-            }  else {
-               if(a_us==true && b_us==false){
-                   --b;
-                   --tmp_a_us;
-               } 
-            }
-            if(( a_us==true || a_us==false ) && b_us==true){
-                ++b;
-                --tmp_a_us;
-            }
+
+        while (a < 0) {
+            --b;
+            ++a;
         }
         return b;
     }
