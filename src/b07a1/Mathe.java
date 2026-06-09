b1c2e3c242d61321443edd502b7e1573a7a625be
felixkhaw <felix.knupper@haw-hamburg.de>
2026-06-09 17:35:48 +0200

    b07a1 fertigstellen von mulLoop

--- src/b07a1/Mathe.java
+++ src/b07a1/Mathe.java
@@ -1,7 +1,6 @@
 package b07a1;
 
 public class Mathe {
-    // a=-3 b=-6
     public static int addLoop(int a, int b) {
         while (a > 0) {
             ++b;
@@ -13,19 +12,35 @@ public class Mathe {
         }
         return b;
     }
-    
-    // a=-6, b=-3
+
     public static int subLoop(int a, int b){    // Operation hängt ab von b
-        // Fall: a=-3, b=3 -> -3-3 = -6
         while (b > 0) {
             --b;
             --a;
         }
-        // Fall: a=-3, b=-3 -> -3-(-3) = 0
         while(b<0){
             ++b;
             ++a;
         }
         return a;
+    }
+
+    public static int mulLoop(int a, int b){
+        if (a == 0 || b == 0) {
+            return 0;
+        }
+        boolean p = (a < 0) == (b < 0);
+        a = Math.abs(a);
+        b = Math.abs(b);
+        int erg = 0;
+        while ( b > 0 ) {
+            int i = a;
+            while( i > 0 ){
+                ++erg;
+                --i;
+            }
+            --b;
+        }
+        return (p) ? erg : -(erg);
     }
 }
