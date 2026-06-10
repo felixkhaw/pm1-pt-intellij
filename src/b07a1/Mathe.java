10612e3c802afbb1c3dc1807a8021de2dfe20b55
felixkhaw <wzr955@haw-hamburg.de>
2026-06-10 08:22:12 +0200

    b07a1 -> Verbessere Formatierung

--- src/b07a1/Mathe.java
+++ src/b07a1/Mathe.java
@@ -1,24 +1,24 @@
 package b07a1;
 
 public class Mathe {
-    public static int addLoop(int a, int b) {
+    public static int addLoop( int a, int b ) {
         while (a > 0) {
             ++b;
             --a;
         }
-        while (a < 0) {
+        while ( a < 0 ) {
             --b;
             ++a;
         }
         return b;
     }
 
-    public static int subLoop(int a, int b){    // Operation hängt ab von b
-        while (b > 0) {
+    public static int subLoop( int a, int b ){    // Operation hängt ab von b
+        while ( b > 0 ) {
             --b;
             --a;
         }
-        while(b<0){
+        while( b < 0 ){
             ++b;
             ++a;
         }
@@ -26,24 +26,24 @@ public class Mathe {
     }
     
     public static int mulLoop(int a, int b) {
-        boolean p = (a < 0) == (b < 0); // Vorzeichen ermitteln
-        a = Math.abs(a);
-        b = Math.abs(b);
+        boolean p = ( a < 0 ) == ( b < 0 ); // Vorzeichen ermitteln
+        a = Math.abs( a );
+        b = Math.abs( b );
         int c = 0;
         int e = 0;
-        while(c < b) {
-            e = addLoop(e, a);
+        while( c < b ) {
+            e = addLoop( e, a );
             ++c;
         }
-        return p ? e : -(e);
+        return p ? e : -( e );
     }
-    public static int divLoop(int a, int b){
+    public static int divLoop( int a, int b ) {
         if( b == 0 ) {
             return -1;
         }
         boolean p = (a < 0) == (b < 0); // Vorzeichen ermitteln
-        a = Math.abs(a);
-        b = Math.abs(b);
+        a = Math.abs( a );
+        b = Math.abs( b );
         int c = 0;
         int e = 0;
         int r = a;
@@ -51,6 +51,6 @@ public class Mathe {
             r = subLoop( r , b );
             ++e;
         }
-        return p ? e : -(e);
+        return p ? e : -( e );
     }
 }
