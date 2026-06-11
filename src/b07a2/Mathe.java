720bc3eece12feac3fe6894dd1d3bb659ec5a65b
felixkhaw <wzr955@haw-hamburg.de>
2026-06-11 11:28:56 +0200

    b07a2 -> Fertigstellen von mulRek() und Helferfunktion

--- src/b07a2/Mathe.java
+++ src/b07a2/Mathe.java
@@ -19,4 +19,17 @@ public class Mathe {
         }
         return a;
     }
+    public static int mulRek(int a, int b){
+        boolean p = ( a < 0 ) == ( b < 0 );
+        a = a < 0 ? -a : a;
+        b = b < 0 ? -b : b;
+        int e = mulRekPositiv(a, b);
+        return p ? e : -e;
+    }
+    private static int mulRekPositiv(int a, int b){
+        if ( b == 0 ) {
+            return 0;
+        }
+        return addRek( a, mulRekPositiv(a, --b) );
+    }
 }
--- src/b07a2/MatheTest.java
+++ src/b07a2/MatheTest.java
@@ -1,4 +1,79 @@
 package b07a2;
 
 public class MatheTest {
+
+    public static boolean addRekCheck(int a, int b, int erw){
+        int erg = Mathe.addRek(a, b);
+        boolean istKorrekt = (erg == erw);
+        if (!istKorrekt) {
+            System.out.println("FEHLER: addRek(" + a + "," + b + ")==" + erg + " statt " + erw);
+        }
+        return istKorrekt;
+    }
+
+    public static boolean addRekTest(){
+        return (
+                addRekCheck(3, 6, 9) &
+                addRekCheck(-3, 6, 3) &
+                addRekCheck(3, -6, -3) &
+                addRekCheck(-3, -6, -9) &
+                addRekCheck(0, 6, 6)
+        );
+    }
+
+    public static boolean subRekCheck(int a, int b, int erw){
+        int erg = Mathe.subRek(a, b);
+        boolean istKorrekt = (erg == erw);
+        if (!istKorrekt) {
+            System.out.println("FEHLER: subRek(" + a + "," + b + ")==" + erg + " statt " + erw);
+        }
+        return istKorrekt;
+    }
+
+    public static boolean subRekTest(){
+        return (
+                subRekCheck(6, 3, 3) &
+                subRekCheck(6, -3, 9) &
+                subRekCheck(-3, 3, -6) &
+                subRekCheck(-3, -3, 0) &
+                subRekCheck(0, 5, -5) &
+                subRekCheck(0, -5, 5) &
+                subRekCheck(5, 0, 5)
+        );
+    }
+
+    public static boolean mulRekCheck(int a, int b, int erw){
+        int erg = Mathe.mulRek(a, b);
+        boolean istKorrekt = (erg == erw);
+        if (!istKorrekt) {
+            System.out.println("FEHLER: mulRek(" + a + "," + b + ")==" + erg + " statt " + erw);
+        }
+        return istKorrekt;
+    }
+
+    public static boolean mulRekTest(){
+        return (
+                mulRekCheck(3, 4, 12) &
+                mulRekCheck(-3, 4, -12) &
+                mulRekCheck(3, -4, -12) &
+                mulRekCheck(-3, -4, 12) &
+                mulRekCheck(0, 5, 0) &
+                mulRekCheck(5, 0, 0) &
+                mulRekCheck(0, -5, 0)
+        );
+    }
+
+    public static boolean test(){
+        return (
+                addRekTest() &
+                subRekTest() &
+                mulRekTest()
+        );
+    }
+
+    public static void main(String[] args) {
+        if( test() ){
+            System.out.println("OK");
+        }
+    }
 }
