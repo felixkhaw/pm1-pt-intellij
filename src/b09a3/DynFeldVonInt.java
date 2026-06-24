50336b65d1287b7de6a45c7c8d688d364bbd6389
felixkhaw <felix.knupper@haw-hamburg.de>
2026-06-25 00:19:09 +0200

    Fertigstellen von b09a3

--- src/b09a3/DynFeldVonInt.java
+++ src/b09a3/DynFeldVonInt.java
@@ -4,27 +4,64 @@ public class DynFeldVonInt {
     private int[] dynarr;
 
     DynFeldVonInt(int al){
-        this.dynarr = new int[al];
+        dynarr = new int[al];
     }
 
     public int[] insert(int i, int k) {
-        int[] tmpArr = new int[this.dynarr.length+k];
-        for(int u = 0; u < this.dynarr.length; ++u){
+        int[] tmpArr = new int[dynarr.length+k];
+        for(int u = 0; u < dynarr.length; ++u){
                 if(u < i){
-                    tmpArr[u] = this.dynarr[u];
+                    tmpArr[u] = dynarr[u];
                 } else {
-                    tmpArr[u+k] = this.dynarr[u];
+                    tmpArr[u+k] = dynarr[u];
                 }
         }
-        this.dynarr = tmpArr;
-        return this.dynarr;
+        dynarr = tmpArr;
+        return dynarr;
     }
 
     public int length(){
        return dynarr.length;
     }
 
-    public int get(){
-        return 0;
+    public int get(int i){
+        return dynarr[i];
+    }
+
+    public void set(int i, int v){
+        dynarr[i] = v;
+    }
+
+    public void remove(int i, int k){
+        if( k<=0 ) throw new IllegalArgumentException("k darf nicht kleiner als 0 sein!");
+        if( i < 0 || i >= dynarr.length ) throw new ArrayIndexOutOfBoundsException(i);
+        if( i+k > dynarr.length ) throw new IllegalArgumentException("Es existieren keine k Elemente an Position i!");
+        int[] tmpArr = new int[ dynarr.length-k ];
+        for(int u = 0; u < dynarr.length-k; ++u){
+            if(u < i){
+                tmpArr[u] = dynarr[u];
+            } else if(u >= i) {
+                tmpArr[u] = dynarr[u+k];
+            }
+        }
+        dynarr = tmpArr;
+    }
+
+    public boolean equals(DynFeldVonInt df){
+        if(df.length() != dynarr.length) return false;
+        for(int i = 0; i < df.length(); ++i){
+            if(dynarr[i] != df.get(i)){
+                return false;
+            }
+        }
+        return true;
+    }
+    public String toString(){
+        String result = "[";
+        for(int p = 0; p < dynarr.length; ++p){
+            result = result + (p == 0 ? "" : " ,") + dynarr[p];
+        }
+        result += "]";
+        return result;
     }
 }
