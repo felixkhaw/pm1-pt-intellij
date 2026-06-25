f5c74b8a79d63f7b9d87761751bef9fafd49f49b
felixkhaw <felix.knupper@haw-hamburg.de>
2026-06-25 23:08:48 +0200

    Bis equals die Funktionen gebaut von b09a4

--- src/b09a4/Matrix.java
+++ src/b09a4/Matrix.java
@@ -26,5 +26,49 @@ public class Matrix {
         }
     }
 
+    public int zeilen(){
+        return m.length;
+    }
 
+    public int spalten(){
+        return m[0].length;
+    }
+
+    public Matrix set(int z, int s, double e){
+        m[z][s] = e;
+        return this;
+    }
+
+    public Matrix setAll(double e){
+        for(int a = 0; a < m.length; ++a ){
+            for(int i = 0; i < m[0].length; ++i){
+                m[a][i] = e;
+            }
+        }
+        return this;
+    }
+
+    public Matrix setZeile(int z, double[] f){
+        if(m[z].length != f.length){
+            throw new IllegalArgumentException("Ungültige Länge des Arrays");
+        }
+        for( int a = 0; a < f.length; ++a ){
+            m[z][a] = f[a];
+        }
+        return this;
+    }
+
+    public Matrix setSpalte(int s, double[] f){
+        if(m.length != f.length){
+            throw new IllegalArgumentException("Ungültige Länge des Arrays");
+        }
+        for( int a = 0; a < f.length; ++a ){
+            m[a][s] = f[a];
+        }
+        return this;
+    }
+    public boolean equals(Matrix m){
+        // liefert als Wahrheitswert, ob die Matrix in Struktur und Werten gleich der
+        //als Argument gegebenen Matrix ist.
+    }
 }
